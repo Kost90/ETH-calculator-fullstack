5d5c99490e5685a99e72071887292d1df920b58e
kost90 <kostdonc90@gmail.com>
2024-07-11 14:17:57 +0100

    edited server folder and socket.hook file

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,7 +18,6 @@ const socket_io_1 = require("socket.io");
 const ws_1 = require("ws");
 const http_1 = __importDefault(require("http"));
 const cors_1 = __importDefault(require("cors"));
-const CryptopricesController_1 = require("./controller/CryptopricesController");
 dotenv_1.default.config();
 const url = process.env.URL_BINANCE;
 // Express server
@@ -22,29 +30,15 @@ const server = http_1.default.createServer(app);
 const ws = new ws_1.WebSocket(url);
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "https://eth-calculator-fullstack.vercel.app",
+        origin: "*",
         methods: ["GET"],
     },
 });
-// Soket connection
-io.on("connection", (soket) => {
-    let intervalId = null;
-    soket.on("data", (data) => {
-        function getData() {
-            return (0, CryptopricesController_1.getCryptoPrices)();
-        }
-        intervalId = setInterval(() => {
-            const price = getData();
-            soket.broadcast.emit("received_prices", price);
-        }, 5000);
-    });
-    // Listen for disconnect event
-    soket.on("disconnect", () => {
-        if (intervalId) {
-            clearInterval(intervalId);
-        }
-    });
-});
+ws.on("message", (event) => __awaiter(void 0, void 0, void 0, function* () {
+    let parse = yield JSON.parse(event.toString());
+    let data = parseFloat(parse.p);
+    io.timeout(5000).emit("received_prices", data);
+}));
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- server/src/routes/Cryptoprices.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getCryptoPrices } from "../controller/CryptopricesController";
-const router = express.Router();
-
-router.get("/", getCryptoPrices);
-
-export default router
