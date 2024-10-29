6901dfc3efa05c9de8462b488569e688de308ef5
Volodymyr Ivanus <ivanys230796@gmail.com>
2024-10-29 12:16:44 +0000

    edited websocket logic

--- client/src/hooks/socket/Socket.tsx
+++ client/src/hooks/socket/Socket.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-const useSocket = (url:string) => {
+const useSocket = (url: string) => {
   const [data, setData] = useState(0);
 
   useEffect(() => {
     const socket = new WebSocket(url);
 
-    const handleReceivedMessage = (event: { data: string; }) => {
+    const handleReceivedMessage = (event: { data: string }) => {
       const parsedData = JSON.parse(event.data);
       const price = Number(parsedData.p);
       setData(parseFloat(price.toFixed(2)));
